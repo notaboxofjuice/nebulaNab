0ec8755e6eef8a85ddc3c14aecc52dd4b87d6f36
Norsefey <129870643+Norsefey@users.noreply.github.com>
2023-10-19 19:05:49 -0400

    Room Management Layout

--- Assets/Scripts/RoomManagementScripts/ConnectionManager.cs
+++ Assets/Scripts/RoomManagementScripts/ConnectionManager.cs
@@ -5,9 +5,7 @@ using Photon.Pun;
 using UnityEngine.SceneManagement;
 
 public class ConnectionManager : MonoBehaviourPunCallbacks
-{
-
-
+{//connects player to server
 
     // Start is called before the first frame update
     void Start()
