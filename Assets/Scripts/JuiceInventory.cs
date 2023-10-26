7f8a5de438dc09fe591bc753368324cc7025d413
Leeman <leemanonline@gmail.com>
2023-10-26 18:26:05 -0400

    Update JuiceInventory.cs

--- Assets/Scripts/JuiceInventory.cs
+++ Assets/Scripts/JuiceInventory.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+/// <summary>
+/// Can be assigned to any object that needs a juice inventory
+/// </summary>
 public class JuiceInventory : MonoBehaviour
 {
     public int juiceCount;
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) // for when assigned to player
     {
         if (collision.gameObject.CompareTag("Juice"))
         {
