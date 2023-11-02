deeac6de8019270db8672922dbe6b75ed4099ec0
Leeman <leemanonline@gmail.com>
2023-11-02 19:09:31 -0400

    Update JuiceInventory.cs
    method for decrementing singleton juice counter

--- Assets/Scripts/JuiceInventory.cs
+++ Assets/Scripts/JuiceInventory.cs
@@ -10,9 +10,15 @@ public class JuiceInventory : MonoBehaviour
         if (other.gameObject.CompareTag("Player") && this.gameObject.CompareTag("Juice"))
         {
             Debug.Log(other.gameObject.name + " is collecting " + juiceCount + " juice."); // debug log
+<<<<<<< Updated upstream
             other.gameObject.GetComponent<JuiceInventory>().juiceCount += juiceCount;
 
             Destroy(gameObject);
+=======
+            other.gameObject.GetComponent<JuiceInventory>().juiceCount += juiceCount; // add juice to player
+            ObjectSpawner.instance.juiceObjectCount--; // decrement the juice object count
+            Destroy(gameObject); // destroy the juice object
+>>>>>>> Stashed changes
         }
     }
 }
