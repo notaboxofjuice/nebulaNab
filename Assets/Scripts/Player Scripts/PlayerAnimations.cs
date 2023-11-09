d24f22c6c502607c4521c05ab35521319c631b76
Norsefey <129870643+Norsefey@users.noreply.github.com>
2023-11-09 18:38:00 -0500

    Baby step 2

--- Assets/Scripts/Player Scripts/PlayerAnimations.cs
+++ Assets/Scripts/Player Scripts/PlayerAnimations.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Animation : MonoBehaviour
+public class PlayerAnimations : MonoBehaviour
 {
     [SerializeField]
     private Animator anime;
