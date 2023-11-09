36979bfcaeb28e7c4bb8599eb92fa1a5445bf258
NSCarlisle <nathan.carlisle95@gmail.com>
2023-11-09 18:13:16 -0500

    asteroid script update
    asteroids now move upward and kill themselves after set time

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] float travelSpeed;
+    [Tooltip("The maximum size the asteroid reaches at the midpoint of it's travel.")]
+    [SerializeField] float maxSize;
+    [Tooltip("Maximum amount of time the object is allowed to persist before it destroys itself.")]
+    [SerializeField] float killTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, killTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + (travelSpeed * Time.deltaTime), transform.position.z);
+    }
+}
