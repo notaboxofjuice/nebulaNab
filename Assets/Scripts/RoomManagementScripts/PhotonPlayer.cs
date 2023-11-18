587866808f0f8d75b45a40921ec90edd36dcd07f
Norsefey <129870643+Norsefey@users.noreply.github.com>
2023-11-18 17:18:06 -0500

    PLayer fix

--- Assets/Scripts/RoomManagementScripts/PhotonPlayer.cs
+++ Assets/Scripts/RoomManagementScripts/PhotonPlayer.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PhotonPlayer : MonoBehaviour
+public class PhotonPlayer : MonoBehaviourPunCallbacks
 {
 
     public static PhotonPlayer Instance;
